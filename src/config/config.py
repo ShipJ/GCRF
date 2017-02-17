01583d27ccd94b5f343d081e68cb2ff7e92b441f
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-02-17 15:24:30 +0000

    Added get_country definition for config file, rather than redefining in each

---
+++ src/config/config.py
@@ -0,0 +1,17 @@
+def get_country():
+    """
+    Ask user for country code.
+
+    :return: str - country for which there is data.
+    """
+    print "Process data for which country? ['sen': Senegal, 'civ': Ivory Coast]: "
+    input_country = raw_input()
+    if input_country == 'sen':
+        country = 'sen'
+    elif input_country == 'civ':
+        country = 'civ'
+    else:
+        print "Please type the country abbreviation (lower case): "
+        return get_country()
+    return country
+
