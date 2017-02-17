a0ea80e585186637217bd7dbcaeec5bef65db47f
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-02-17 12:25:02 +0000

    Process raw successfully runs for Country A and B

--- src/data/process_raw.py
+++ src/data/process_raw.py
@@ -1,49 +1,46 @@
 import pandas as pd
 import tarfile
-import zipfile
-import glob
-import sys
 import os
+import sys
 
 
-def process_raw(source, target):
+def process_raw(source, target, country):
     """
     From raw zip/tar files containing CDR data, extract tsv's,
     split files by timestamp, store in separate files.
 
-    :param source: string - file path to raw data.
-    :param target: string - file path to save processed data.
-    :param country: string - country code.
+    :param source: str - file path to raw data.
+    :param target: str - file path to save processed data.
+    :param country: str - country code.
 
     :return: None.
     """
-    tar = tarfile.open(source, 'r')
+    if country == 'civ':
+        file = tarfile.open(source+'.zip', 'r')
+        for f in file:
+            print "Reading %s..." % f.name
+            df = pd.DataFrame.from_csv(file.extractfile(f), sep='\t', header=None).reset_index()
+            df.columns = ['datetime', 'source', 'target', 'activity', 'duration']
+            # Split file by timestamp
+            time_stamped = df.groupby('datetime')
+            save_timestamp(time_stamped, target)
 
-    for file in tar:
-        print "Reading %s ..." % tar.name
-
-        df = pd.DataFrame.from_csv(source+file.name, sep='\t', header=None).reset_index()
-        df.columns = ['datetime', 'source', 'target', 'activity', 'duration']
-
-        # Split file by timestamp
-        time_stamped = df.groupby('datetime')
-
-        # Save each split to a time-stamped file
-        for name, group in time_stamped:
-            # Grab current datetime
-            current = group['datetime'].iloc[0]
-            # Extract datetime as strings
-            y, m, d, h = str(current.year), str(current.month), str(current.day), str(current.hour)
-            print "Reading %s-%s-%s-%s data" % (y, m, d, '{0:0>2}'.format(h))
-            # Save time-stamped file
-            group.to_csv(target+'/%s-%s-%s-%s.csv' % (y, m, d, '{0:0>2}'.format(h)), index=None)
+    elif country == 'sen':
+        file = [i for i in os.listdir(source) if i.startswith('SET1V')]
+        for f in file:
+            print "Reading %s..." % f
+            df = pd.DataFrame.from_csv(source+'/'+f, sep=',', header=None).reset_index()
+            df.columns = ['datetime', 'source', 'target', 'activity', 'duration']
+            # Split file by timestamp
+            time_stamped = df.groupby('datetime')
+            save_timestamp(time_stamped, target)
 
 
 def get_country():
     """
-    Ask for user to input a country.
+    Ask user for country code.
 
-    :return: string - country for which there is data.
+    :return: str - country for which there is data.
     """
     print "Process data for which country? ['sen': Senegal, 'civ': Ivory Coast]: "
     input_country = raw_input()
@@ -59,9 +56,9 @@ def get_country():
 
 def get_data_set():
     """
-    Ask the user which data set they would like to process
+    Ask user for data set to process.
 
-    :return: string - data set reference
+    :return: str - data set reference
     """
     print "Process which data set? [1, 2, 3 or 4?]: "
     data_set = raw_input()
@@ -72,14 +69,32 @@ def get_data_set():
         return get_data_set()
 
 
-if __name__ == '__main__':
+def save_timestamp(time_stamped, target):
+    """
+    Take time-stamped data and store in target file
 
-    # Retrieve country code and data set to process
+    :param time-stamped: pandas groupby object - from process_raw()
+    :param target: str - store in interim data files
+
+    :return: None
+    """
+    for name, group in time_stamped:
+        # Grab current datetime
+        current = group['datetime'].iloc[0]
+        # Extract datetime as strings
+        y, m, d, h = str(current.year), str(current.month), str(current.day), str(current.hour)
+        print "Reading %s-%s-%s-%s data" % (y, m, d, '{0:0>2}'.format(h))
+        # Store as a time-stamped file
+        group.to_csv(target + '/%s-%s-%s-%s.csv' % (y, m, d, '{0:0>2}'.format(h)), index=None)
+
+
+if __name__ == '__main__':
+    # Ask user for country code and data set
     country = get_country()
     data_set = get_data_set()
 
-    # Grab from source, process, save to target
-    source = '../../data/raw/%s/CDR%s.zip' % (country, data_set)
-    target = '../../data/processed/%s/CDR/timestamp/' % country
+    # Grab data from raw, process, save to interim
+    source = '../../data/raw/%s/CDR%s' % (country, data_set)
+    target = '../../data/interim/%s/CDR/timestamp/' % country
 
-    process_raw(source, target)
+    process_raw(source, target, country)
