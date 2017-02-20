introversion was returnign wrong values

src/data/merge_cdr_dhs.py
@@ -4,40 +4,49 @@ import numpy as np
 import sys
 
 
-country = config.get_country()
+if __name__ == '__main__':
+    country = config.get_country()
+    cdr_metrics = config.get_cdr_metrics(country)
 
-activity = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/staticmetrics/new/total_activity.csv' % country))
-entropy = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/staticmetrics/new/entropy.csv' % country))
-med_degree = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/staticmetrics/new/med_degree.csv' % country))
-graph_metrics = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/staticmetrics/new/graph_metrics.csv' % country))
-introversion = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/staticmetrics/new/introversion.csv' % country))
+    print cdr_metrics
 
-master = activity.merge(entropy,
-                        on='CellTowerID').merge(med_degree,
-                                                on='CellTowerID').merge(graph_metrics,
-                                                                        on='CellTowerID').merge(introversion,
-                                                                                                on='CellTowerID')
 
-# Merge Adm regions
-cdr_adm = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/CellTowers/CDR_Adm_1234.csv' % country,
-                                   usecols=['CellTowerID', 'Adm_1', 'Adm_2', 'Adm_3', 'Adm_4']))
+
+
+    sys.exit()
+
+
+
+
+
+    # Merge Adm region
+    cdr_adm = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/CellTowers/CDR_Adm_1234.csv' % country,
+                                       usecols=['CellTowerID', 'Adm_1', 'Adm_2', 'Adm_3', 'Adm_4']))
 
 master = master.merge(cdr_adm, on='CellTowerID')
 
 ''' merge populations and distance data '''
-pop = pd.DataFrame(pd.read_csv('../../data/processed/%s/pop/IntersectPop.csv' % country))
-pop_10 = pop.groupby('CellTowerID')['Pop_2010'].sum().reset_index()
-pop_14 = pop.groupby('CellTowerID')['Pop_2014'].sum().reset_index()
+intersect_pop = pd.DataFrame(pd.read_csv('../../data/processed/%s/pop/IntersectPop.csv' % country))
+pop_10 = intersect_pop.groupby('CellTowerID')['Pop_2010'].sum().reset_index()
+pop_14 = intersect_pop.groupby('CellTowerID')['Pop_2014'].sum().reset_index()
 
 master = master.merge(pop_10, on='CellTowerID').merge(pop_14, on='CellTowerID')
 
 constants = config.get_constants(country)
 num_towers = constants['num_towers']
 
+pop_4 = config.get_pop(country, 4)
 
+vor_prop = np.zeros(len(intersect_pop))
+for i in range(len(intersect_pop)):
+    adm_4 = int(intersect_pop.iloc[i].Adm_4)
+    pop_adm_4 = pop_4[pop_4['Adm_4'] == adm_4]['Pop_2010']
 
+    vor_prop[i] = intersect_pop.iloc[i].Pop_2010 / pop_adm_4
 
+intersect_pop['vorProp'] = vor_prop
 
+master_adm = pd.DataFrame(pd.read_csv('../../data/processed/%s/cdr/celltowers/Adm_1234.csv' % country))
 
 
 

src/features/cdr/static/cdr_metrics.py
@@ -123,10 +123,10 @@ def introversion(num_towers, adj_matrix):
     for i in range(num_towers):
         out = np.sum(np.delete(adj_matrix[i, :], i))
         introv[i] = (adj_matrix[i, i] / out) if out > 0 else 0
-    introv = pd.DataFrame()
-    introv['CellTowerID'] = np.array(range(num_towers))
-    introv['Introversion'] = introv
-    return introv
+    introverse = pd.DataFrame()
+    introverse['CellTowerID'] = np.array(range(num_towers))
+    introverse['Introversion'] = introv
+    return introverse
 
 
 def graph_metrics(adj_matrix):
@@ -180,11 +180,11 @@ if __name__ == '__main__':
     adj_matrix_dur = np.genfromtxt('../../../../data/processed/%s/cdr/staticmetrics/adj_matrix_dur.csv' % country,
                                    delimiter=',')
 
-    total_activity = activity(country, num_towers, adj_matrix_vol, adj_matrix_dur)
-    deg_vector = degree_vector(country, num_towers, adj_matrix_vol)
-    entropy = entropy(country, num_towers, adj_matrix_vol)
-    med_deg = med_degree(country, num_towers, adj_matrix_vol)
-    graph = graph_metrics(adj_matrix_vol)
+    # total_activity = activity(country, num_towers, adj_matrix_vol, adj_matrix_dur)
+    # deg_vector = degree_vector(country, num_towers, adj_matrix_vol)
+    # entropy = entropy(country, num_towers, adj_matrix_vol)
+    # med_deg = med_degree(country, num_towers, adj_matrix_vol)
+    # graph = graph_metrics(adj_matrix_vol)
     introv = introversion(num_towers, adj_matrix_vol)
     # gravity = gravity(adj_matrix_vol, dist_matrix, pop)
     # radiation = radiation(adj_matrix_vol, dist_matrix, pop)
@@ -198,8 +198,8 @@ if __name__ == '__main__':
     #                index=None)
     # med_deg.to_csv('../../../../data/processed/%s/cdr/staticmetrics/new/med_degree.csv' % country,
     #                index=None)
-    # introv.to_csv('../../../../data/processed/%s/cdr/staticmetrics/new/introversion.csv' % country,
-    #                index=None)
+    introv.to_csv('../../../../data/processed/%s/cdr/staticmetrics/new/introversion.csv' % country,
+                   index=None)
     # graph.to_csv('../../../../data/processed/%s/cdr/staticmetrics/new/graph_metrics.csv' % country,
     #              index=None)
     # gravity.to_csv('../../../../data/processed/%s/cdr/staticmetrics/new/gravity.csv' % country,