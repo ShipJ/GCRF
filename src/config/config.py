61596628bd2da557ed6d7196122529469707f24b
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-02-23 13:35:18 +0000

    Retrieve DHS data into dataframes for each country

--- src/config/config.py
+++ src/config/config.py
@@ -97,12 +97,12 @@ def get_master(country):
 
 
 def get_dhs(country):
-    malaria = pd.DataFrame(pd.read_csv('../../../data/interim/%s/dhs/malaria.csv' % country))
-    child_mort = pd.DataFrame(pd.read_csv('../../../data/interim/%s/dhs/malaria.csv' % country))
-    women_health_access = pd.DataFrame(pd.read_csv('../../../data/interim/%s/dhs/malaria.csv' % country))
-    preventable_disease = pd.DataFrame(pd.read_csv('../../../data/interim/%s/dhs/malaria.csv' % country))
+    malaria = pd.DataFrame(pd.read_csv('../../data/interim/%s/dhs/malaria.csv' % country))
+    child_mort = pd.DataFrame(pd.read_csv('../../data/interim/%s/dhs/malaria.csv' % country))
+    women_health_access = pd.DataFrame(pd.read_csv('../../data/interim/%s/dhs/malaria.csv' % country))
+    preventable_disease = pd.DataFrame(pd.read_csv('../../data/interim/%s/dhs/malaria.csv' % country))
     if country == 'civ':
-        hiv = pd.DataFrame(pd.read_csv('../../../data/interim/%s/dhs/malaria.csv' % country))
+        hiv = pd.DataFrame(pd.read_csv('../../data/interim/%s/dhs/malaria.csv' % country))
         return [malaria, child_mort, women_health_access, hiv, preventable_disease]
     else:
         return [malaria, child_mort, women_health_access, preventable_disease]
--- src/data/process_dhs.py
+++ src/data/process_dhs.py
@@ -9,9 +9,13 @@ if __name__ == '__main__':
     country = config.get_country()
     constants = config.get_constants(country)
 
-    malaria = np.genfromtxt('../../../data/interim/%s/dhs/.csv' % country, delimiter=',')
+    if country == 'civ':
+        malaria, child_mort, women_health_access, hiv, preventable_disease = config.get_dhs(country)
+    else:
+        malaria, child_mort, women_health_access, preventable_disease = config.get_dhs(country)
 
-    print malaria
+
+
 
 
 
