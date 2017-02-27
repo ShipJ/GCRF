f3c03f4a53853123195b7b502e762d33d50bdb4f
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-02-27 23:06:44 +0000

    Volatility computing

--- src/metrics/cdr/static/temporal_adj_matrix.py
+++ src/metrics/cdr/static/temporal_adj_matrix.py
@@ -5,12 +5,11 @@ import scipy.sparse as sparse
 from src.config import config
 
 
-def adj_matrix(source, target, country):
+def adj_matrix(source, country):
     """
     From time-stamped data files, compute temporal adjacency matrix: runtime {3m, 15m}
 
     :param source: string - file path to raw data.
-    :param target: string - file path to save processed data.
     :param country: str - country code.
 
     :return: None.
@@ -18,29 +17,29 @@ def adj_matrix(source, target, country):
 
     constants = config.get_constants(country)
     num_towers = constants['num_towers']
-    hours = constants['hours']
+    num_hours = constants['hours']
 
     temporal_adj_vol = []
     temporal_adj_dur = []
 
     i = 0
     for f in os.listdir(source):
-        print "Reading: %s" % f
+        if f not in ['data.txt', '.DS_Store']:
+            print "Reading: %s" % f
 
-        # A 'coo' matrix is a coordinate based sparse matrix (efficient/fast)
-        coo_vol = sparse.coo_matrix((num_towers, num_towers))
-        coo_dur = sparse.coo_matrix((num_towers, num_towers))
+            # A 'coo' matrix is a coordinate based sparse matrix (efficient/fast)
+            coo_vol = sparse.coo_matrix((num_towers, num_towers, num_hours))
+            coo_dur = sparse.coo_matrix((num_towers, num_towers, num_hours))
 
-        cdr = pd.read_csv((source+f), usecols=['source', 'target', 'activity']).as_matrix()
-        # -1: cdr where origin/target cell tower unknown
-        missing = np.where(cdr == -1)
-        # Convert to imaginary cell tower ID (this is not useful, but aids processing)
-        cdr[missing] = num_towers-1
-        temporal_adj_vol.append(sparse.coo_matrix((cdr[:, 2], (cdr[:, 0], cdr[:, 1])), shape=(num_towers, num_towers)))
-        temporal_adj_dur.append(sparse.coo_matrix((cdr[:, 3], (cdr[:, 0], cdr[:, 1])), shape=(num_towers, num_towers)))
+            cdr = pd.read_csv((source+f), usecols=['source', 'target', 'activity', 'duration']).as_matrix()
+            # -1: cdr where origin/target cell tower unknown
+            missing = np.where(cdr == -1)
+            # Convert to imaginary cell tower ID (this is not useful, but aids processing)
+            cdr[missing] = num_towers-1
+            temporal_adj_vol.append(sparse.coo_matrix((cdr[:, 2], (cdr[:, 0], cdr[:, 1])), shape=(num_towers, num_towers)))
+            temporal_adj_dur.append(sparse.coo_matrix((cdr[:, 3], (cdr[:, 0], cdr[:, 1])), shape=(num_towers, num_towers)))
 
-    print temporal_adj_vol
-    print len(temporal_adj_vol)
+
 
     # # Sparse to dense matrix (i.e. adjacency matrix)
     # np.savetxt(target+'/adj_matrix_vol.csv', coo_vol.todense().reshape(num_towers, num_towers), delimiter=',')
@@ -49,6 +48,6 @@ def adj_matrix(source, target, country):
 
 if __name__ == '__main__':
     country = config.get_country()
-    source = '../../data/interim/%s/cdr/timestamp/' % country
+    source = '../../../../data/interim/%s/cdr/timestamp/' % country
     target = '../../data/processed/%s/cdr/staticmetrics' % country
-    adj_matrix(source, target, country)
+    adj_matrix(source, country)
