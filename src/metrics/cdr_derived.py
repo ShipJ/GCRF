fcc6119930d38f6f418da1dbc99453e9f29b360b
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-03-10 16:24:32 +0000

    renamed distance directory to include area

--- src/metrics/cdr_derived.py
+++ src/metrics/cdr_derived.py
@@ -14,13 +14,25 @@ if __name__ == '__main__':
 
     cdr_fundamentals = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_fundamentals_adm.csv' % country))
 
-    # Add the population within each adm region
+    # Population of each adm region
     cdr_fundamentals = cdr_fundamentals.merge(config.get_pop(country, 4), on='Adm_4')
+    # Area in m^2 and km^2 of each adm region
+    area = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/distance_area/area.csv' % country, usecols=['Adm_4',
+                                                                                                    'Area_m2',
+                                                                                                    'Area_km2']))
+    cdr_fundamentals = cdr_fundamentals.merge(area, on='Adm_4', how='outer')
 
     # Log volume
     cdr_fundamentals['Log_vol'] = np.log(cdr_fundamentals['Vol'])
-
     # Volume per person
     cdr_fundamentals['Vol_pp'] = cdr_fundamentals['Vol']/cdr_fundamentals['Pop_2010']
+    # Population density
+    cdr_fundamentals['Pop_density'] = cdr_fundamentals['Pop_2010']/cdr_fundamentals['Area_km2']
+    # Log(Pop density)
+    cdr_fundamentals['Log_pop_density'] = np.log(cdr_fundamentals['Pop_density'])
+
+    cdr_derived = pd.DataFrame(cdr_fundamentals)
+
+    cdr_derived.to_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_adm.csv' % country, index=None)
 
 
---
+++ src/metrics/merge.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from src.config import config
+
+if __name__ == '__main__':
+    PATH = config.get_dir()
+    country = config.get_country()
+
+    cdr = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_adm.csv' % country))
+    dhs = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/dhs/dhs_fundamentals_adm.csv' % country))
+    # spatial_lag = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/spatial_lag.csv' % country))
+    # other = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/dhs/other_fundamentals_adm.csv' % country))
+
+    master = pd.DataFrame(cdr.merge(dhs, on=['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4'], how='outer'))
+    master.to_csv(PATH+'/final/%s/master_1.0.csv', index=None)
