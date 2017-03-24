588008d13a29c3395703aeff1a7b4fef82641d15
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-03-24 11:46:28 +0000

    Updated config file with latest PATH links

--- src/metrics/cdr_derived.py
+++ src/metrics/cdr_derived.py
@@ -12,18 +12,16 @@ if __name__ == '__main__':
     PATH = config.get_dir()
     country = config.get_country()
 
-    cdr_fundamentals = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_fundamentals_adm.csv' % country))
+    cdr_fundamentals = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_fundamentals_bts_all.csv'
+                                                % country))
 
-    # Population of each adm region
+    # Population of each adm region - region 4 accounts for regions 1, 2 and 3
     cdr_fundamentals = cdr_fundamentals.merge(config.get_pop(country, 4), on='Adm_4')
     # Area in m^2 and km^2 of each adm region
     area = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/distance_area/area.csv' % country, usecols=['Adm_4',
                                                                                                     'Area_m2',
                                                                                                     'Area_km2']))
     cdr_fundamentals = cdr_fundamentals.merge(area, on='Adm_4', how='outer')
-
-    # Log volume
-    cdr_fundamentals['Log_vol'] = np.log(cdr_fundamentals['Vol'])
     # Volume per person
     cdr_fundamentals['Vol_pp'] = cdr_fundamentals['Vol']/cdr_fundamentals['Pop_2010']
     # Population density
@@ -31,8 +29,7 @@ if __name__ == '__main__':
     # Log(Pop density)
     cdr_fundamentals['Log_pop_density'] = np.log(cdr_fundamentals['Pop_density'])
 
-    cdr_derived = pd.DataFrame(cdr_fundamentals)
+    cdr_fundamentals.to_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_bts_all.csv' % country, index=None)
 
-    cdr_derived.to_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_adm.csv' % country, index=None)
 
 
