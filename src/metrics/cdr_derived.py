064de7bacc649de42bbe84ccaf14b590ef222bc2
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-04-06 15:14:05 +0100

    Successfully computes derived metrics

--- src/metrics/cdr_derived.py
+++ src/metrics/cdr_derived.py
@@ -9,33 +9,32 @@ from src.config import config
 import sys
 
 if __name__ == '__main__':
+    # System path to data directory
     PATH = config.get_dir()
+    # Ask user for country
     country = config.get_country()
 
+    # Currently for all hours -> future, add 'working hours' etc.
     for i in ['all']:
-        cdr_fundamentals = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_fundamentals_ct_%s.csv'
-                                                    % (country, i)))
+        cdr_aggregated = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/cdr/metrics/cdr_aggregate_adm_%s.csv'
+                                                  % (country, i)))
 
-        # Population of each adm region
+        # Population of adm regions
         ct_pop = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/pop/intersect_pop.csv' % country))
-        ct_pop = ct_pop.groupby('CellTowerID')['Pop_2010'].sum().reset_index()
+        ct_pop = ct_pop.groupby('Adm_4')['Pop_2010', 'Pop_2014'].sum().reset_index()
 
         # Area in m^2 and km^2 of each adm region
-        area = pd.DataFrame(pd.read_csv(PATH + '/processed/%s/geo/area.csv' % country,
+        area = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/geo/area.csv' % country,
                                         usecols=['Adm_4', 'Area_m2', 'Area_km2']))
 
-        print area
-
-        sys.exit()
-        cdr_fundamentals = cdr_fundamentals.merge(ct_pop, on='CellTowerID', how='outer').merge(area, on='Adm_4')
+        # Merge area and population data
+        cdr_derived = cdr_aggregated.merge(area, on='Adm_4').merge(ct_pop, on='Adm_4', how='outer')
         # Volume per person
-        cdr_fundamentals['Vol_pp'] = cdr_fundamentals['Vol']/cdr_fundamentals['Pop_2010']
+        cdr_derived['Vol_pp'] = cdr_derived['Vol']/cdr_derived['Pop_2010']
         # Population density
-        cdr_fundamentals['Pop_density'] = cdr_fundamentals['Pop_2010']/cdr_fundamentals['Area_km2']
+        cdr_derived['Pop_density'] = cdr_derived['Pop_2010']/cdr_derived['Area_km2']
         # Log(Pop density)
-        cdr_fundamentals['Log_pop_density'] = np.log(cdr_fundamentals['Pop_density'])
+        cdr_derived['Log_pop_density'] = np.log(cdr_derived['Pop_density'])
 
-        cdr_fundamentals.to_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_bts_%s.csv' % (country, i), index=None)
-
-
-
+        # Save derived metrics to csv
+        cdr_derived.to_csv(PATH+'/processed/%s/cdr/metrics/cdr_derived_ct_%s.csv' % (country, i), index=None)
