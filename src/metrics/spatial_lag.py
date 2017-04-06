d12ac31a897d77f24a631a947bfe425d9d5275d3
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-04-06 16:01:09 +0100

    Spatial lag successfully runs on testing

--- src/metrics/spatial_lag.py
+++ src/metrics/spatial_lag.py
@@ -6,24 +6,18 @@ from src.config import config
 if __name__ == '__main__':
     PATH = config.get_dir()
     country = config.get_country()
+    data = pd.DataFrame(pd.read_csv(PATH+'/final/%s/master.csv' % country))
 
     if country == 'civ':
-        dhs = pd.DataFrame(
-            pd.read_csv(PATH + '/final/%s/master_1.0.csv' % country, usecols=['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4',
-                                                                              'BloodPosRate',
-                                                                              'RapidPosRate',
-                                                                              'PosRate',
-                                                                              'DeathRate',
-                                                                              'DifficultyScore']))
-        dist = pd.DataFrame(pd.read_csv(PATH + '/processed/%s/distance_area/dist_matrix_adm4.csv' % country))
-        adm = dhs[['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4']]
+        dhs = data[['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4',
+                    'BloodPosRate', 'RapidPosRate', 'PosRate', 'DeathRate', 'DifficultyScore']]
+        dist = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/geo/adm4_dist_matrix.csv' % country))
 
         spatial_lag = []
         for response in ['BloodPosRate', 'RapidPosRate', 'PosRate', 'DeathRate', 'DifficultyScore']:
             spatial_score = []
             for i in dhs['Adm_4']:
                 response_i = dhs[dhs['Adm_4'] == i]
-
                 a = dist[dist['Source'] == i]
                 a_dist = a[a['Distance_km'] > 0]
                 sum_weights = np.sum(np.reciprocal(a_dist['Distance_km'] ** 2))
@@ -33,22 +27,17 @@ if __name__ == '__main__':
                     b = a[a['Target'] == j]
                     sum_lag += ((np.array(b['Distance_km'])[0] / float(sum_weights)) *
                                 np.array(response_i[response])[0])
+
                 spatial_score.append(sum_lag)
             spatial_lag.append(spatial_score)
         spatial = pd.DataFrame(np.transpose(spatial_lag), columns=['BloodPosRateSL', 'RapidPosRateSL', 'DeathRateSL',
                                                                    'PosRateSL', 'DifficultyScoreSL'])
-        spatial = pd.DataFrame(pd.concat([adm, spatial], axis=1))
-        spatial.to_csv(PATH + '/processed/%s/cdr/metrics/spatial_lag_adm_all.csv' % country, index=None)
+        spatial = pd.DataFrame(pd.concat([data, spatial], axis=1))
+        spatial.to_csv(PATH+'/final/%s/master_SL.csv' % country, index=None)
 
     elif country == 'sen':
-        dhs = pd.DataFrame(
-            pd.read_csv(PATH + '/final/%s/master_1.0.csv' % country, usecols=['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4',
-                                                                              'BloodPosRate',
-                                                                              'RapidPosRate',
-                                                                              'DeathRate']))
-
-        dist = pd.DataFrame(pd.read_csv(PATH + '/processed/%s/distance_area/dist_matrix_adm4.csv' % country))
-        adm = dhs[['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4']]
+        dhs = data[['Adm_1', 'Adm_2', 'Adm_3', 'Adm_4', 'BloodPosRate', 'RapidPosRate', 'DeathRate']]
+        dist = pd.DataFrame(pd.read_csv(PATH+'/processed/%s/geo/adm4_dist_matrix.csv' % country))
 
         spatial_lag = []
         for response in ['BloodPosRate', 'RapidPosRate', 'DeathRate']:
@@ -67,9 +56,10 @@ if __name__ == '__main__':
                                 np.array(response_i[response])[0])
                 spatial_score.append(sum_lag)
             spatial_lag.append(spatial_score)
+
         spatial = pd.DataFrame(np.transpose(spatial_lag), columns=['BloodPosRateSL', 'RapidPosRateSL', 'DeathRateSL'])
-        spatial = pd.DataFrame(pd.concat([adm, spatial], axis=1))
-        spatial.to_csv(PATH+'/final/%s/cdr/metrics/spatial_lag_adm_all.csv' % country, index=None)
+        spatial = pd.DataFrame(pd.concat([data, spatial], axis=1))
+        spatial.to_csv(PATH+'/final/%s/master_SL.csv' % country, index=None)
 
 
 
