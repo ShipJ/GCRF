3f0d9714ec6fba587dd680b4ebc2defff423bcaf
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-04-13 11:06:57 +0100

    Print out particular plots

--- src/analysis/correlation.py
+++ src/analysis/correlation.py
@@ -131,14 +131,13 @@ if __name__ == '__main__':
     group_features = config.get_headers(country, 'group')
 
     group = 'None'
-    models = ['No outliers, no transformation ', 'Outliers, no transformation ',
-              'No outliers, log-log-transformation ', 'Outliers, log-log-transformation ',
-              'Outliers, log-linear-transformation ', 'Outliers, linear-log-transformation ',
-              'Outliers, then log-log ', 'root root', 'noth root', 'root noth', 'root log']
+    models = ['NoOutliersNoTransform', 'OutliersNoTransformation',
+              'NoOutliersLogLogTransform', 'OutliersLogLogTransform',
+              'OutliersLogLinTransform', 'OutliersLinLogTransform']
 
     data = pd.DataFrame(pd.read_csv(PATH+'/final/%s/master_SL.csv' % country))
 
-    for adm in np.setdiff1d(adms, ['Adm_1']):
+    for adm in np.setdiff1d(adms, []):
         dhs_adm = aggregate_mean(data, dhs_features, adm)
         cdr_sum_adm = aggregate_sum(data, cdr_features[:6], adm)
         cdr_mean_adm = aggregate_mean(data, cdr_features[6:], adm)
@@ -161,18 +160,24 @@ if __name__ == '__main__':
                     if country == 'civ':
                         merged = merged.merge(group_adm, on=adm).dropna()
 
-                for i in range(7):
+                for i in range(6):
                     print models[i], adm, j, k
                     transformed = transform(merged.copy(), k, j, i+1, adm)
                     iv, dv = transformed[k], transformed[j]
-                    print pearsonr(iv, dv)
+                    pr = pearsonr(iv, dv)
+                    print pr[0]
                     if group != 'None':
                         if country == 'civ':
                             lower_half = np.array(transformed[group] <= np.median(transformed[group]))
                             upper_half = np.array(transformed[group] > np.median(transformed[group]))
                             group_scatter(iv, dv, lower_half, upper_half, group)
                     else:
-                        plt.scatter(iv, dv,c='black')
-                        plt.xlabel(k), plt.ylabel(j), plt.title(k+' vs. '+j+' : '+adm+'\n'+'('+models[i]+')')
-                        plt.grid()
-                        plt.show()
+                        if abs(pr[0]) > 0.4:
+                            plt.scatter(iv, dv,c='black')
+                            plt.xlabel(k), plt.ylabel(j), plt.title(k+' vs. '+j+' : '+adm+'\n'+'('+models[i]+')')
+                            plt.grid()
+
+                            plt.savefig('/Users/JackShipway/Desktop/Figures/%s/%s_%s_%s_%s.png' %
+                                        (country, adm, k, j, models[i]), dpi=600)
+                            plt.show()
+
